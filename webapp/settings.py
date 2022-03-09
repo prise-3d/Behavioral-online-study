119d7d05731027055e937d6735d2582e928e5902
Jérome Buisine <jerome.buisine@outlook.fr>
2022-03-09 13:05:33 +0100

    remove unused package inside settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import json
-from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
